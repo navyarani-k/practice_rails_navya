fd85940947c28939f54ba505284e8a66594cd923
navyarani-k <navyarani.k@etvbharat.com>
2020-07-24 12:06:53 +0530

    articles page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -38,6 +38,10 @@
   end
   
   def destroy 
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:success] = "your article hasbeen deleted"
+    redirect_to articles_path
   end
   
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ApplicationRecord
 	mount_uploader :image, ImageUploader
 	validates :title, presence: true, length: {minimum: 4, maximum: 100};
 	validates :description, presence: true, length: {minimum: 20, maximum: 500};
-
+    validates :image, presence: true;
 end
 		
